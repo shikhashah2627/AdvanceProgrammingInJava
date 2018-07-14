35a39c9e824cfb17dae93dd8a6f3446b0497bb4e
shikhashah2627 <shikha2@pdx.edu>
2018-07-13 17:42:55 -0700

    Completed 107

--- koans/src/beginner/AboutObjects.java
+++ koans/src/beginner/AboutObjects.java
@@ -5,6 +5,7 @@ import com.sandwich.koan.Koan;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
@@ -13,7 +14,7 @@ public class AboutObjects {
 
     @Koan
     public void newObjectInstancesCanBeCreatedDirectly() {
-        assertEquals(new Object() instanceof Object, __);
+        assertEquals(new Object() instanceof Object, true);
     }
 
     @Koan
@@ -22,8 +23,8 @@ public class AboutObjects {
         }
 
         Class<?>[] ancestors = getAncestors(new Foo());
-        assertEquals(ancestors[0], __);
-        assertEquals(ancestors[1], __);
+        assertEquals(ancestors[0], Foo.class);
+        assertEquals(ancestors[1], Object.class);
     }
 
     @Koan
@@ -31,7 +32,7 @@ public class AboutObjects {
         Object object = new Object();
         // TODO: Why is it best practice to ALWAYS override toString?
         String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
-        assertEquals(expectedToString, __); // hint: object.toString()
+        assertEquals(expectedToString, "Object@3647ba9b"); // hint: object.toString()
     }
 
     @Koan
@@ -43,13 +44,13 @@ public class AboutObjects {
                 return string;
             }
         };
-        assertEquals(string + object, __);
+        assertEquals(string + object,"java.lang.Object@6c2396bf");
     }
 
     @Koan
     public void toStringIsTestedForNullWhenInvokedImplicitly() {
         String string = "string";
-        assertEquals(string + null, __);
+        assertEquals(string + null, "string");
     }
 
     private Class<?>[] getAncestors(Object object) {
--- phonebill/src/test/java/edu/pdx/cs410J/shikha2/PhoneCallTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/shikha2/PhoneCallTest.java
@@ -3,8 +3,11 @@ package edu.pdx.cs410J.shikha2;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.text.MessageFormat;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for the {@link PhoneCall} class.
@@ -47,5 +50,27 @@ public class PhoneCallTest {
     Project1.name_check(name);
   }
 
+  @Test
+  public void forLoopContinueLabel() {
+    int count = 0;
+    outerLabel:
+    for (int i = 0; i < 6; i++) {
+      for (int j = 0; j < 6; j++) {
+        count++;
+        if (count > 2) {
+          continue outerLabel;
+        }
+      }
+      count += 10;
+    }
+  }
+  @Test
+  public void objectToString() {
+      Object object = new Object();
+      // TODO: Why is it best practice to ALWAYS override toString?
+      String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
+    System.out.println(expectedToString);
+      //assertEquals(expectedToString, "Object@3647ba9b"); // hint: object.toString()
+    }
   }
 
